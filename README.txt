c492f386b772883e9b8cc9fcf31a5a65d46e01b2
TerleckiyGP <programmer@zeo.ua>
2020-09-23 13:28:07 +0300

    make default api

--- README.txt
+++ README.txt
@@ -4,3 +4,5 @@
 # curl -X POST localhost:8000/api/login -H "Accept: application/json" -H "Content-type: application/json" -d "{\"email\": \"admin@test.com\", \"password\": \"password\" }"
 
 # curl -X POST http://localhost:8000/api/register -H "Accept: application/json" -H "Content-Type: application/json" -d '{"name": "John", "email": "john.doe@toptal.com", "password": "password", "password_confirmation": "password"}'
+
+# curl -X GET http://localhost:8000/api/user -H "Accept: application/json" -H "Content-Type: application/json" -d
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,35 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+Accept: application/json.
+200: OK. The standard success code and default option.
+201: Object created. Useful for the store actions.
+204: No content. When an action was executed successfully, but there is no content to return.
+206: Partial content. Useful when you have to return a paginated list of resources.
+400: Bad request. The standard option for requests that fail to pass validation.
+401: Unauthorized. The user needs to be authenticated.
+403: Forbidden. The user is authenticated, but does not have the permissions to perform an action.
+404: Not found. This will be returned automatically by Laravel when the resource is not found.
+500: Internal server error. Ideally you're not going to be explicitly returning this, but if something unexpected breaks, this is what your user is going to receive.
+503: Service unavailable. Pretty self explanatory, but also another code that is not going to be returned explicitly by the application.
 */
+//
+Auth::guard('api')->user(); // instance of the logged user
+Auth::guard('api')->check(); // if a user is authenticated
+Auth::guard('api')->id(); // the id of the authenticated user
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login')->name('login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('user', 'UserController@index');
+    Route::get('user/{user}', 'UserController@show');
+    Route::post('user', 'UserController@store');
+    Route::put('user/{user}', 'UserController@update');
+    Route::delete('user/{user}', 'UserController@delete');
 });
